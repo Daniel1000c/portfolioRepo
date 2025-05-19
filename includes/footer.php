c0e20457bd2acba53653912a93598e29ca9b096e
Daniel Velez <velezdaniel516@gmail.com>
2025-05-19 16:20:38 -0400

    add footer links and format footer container

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,15 @@
 </main>
     <footer>
-        <p>&copy; <?php echo date('Y'); ?> Daniel Velez. All Rights Reserved.</p>
+        <div class="container">
+            <hr>
+            <p>&copy; <?php echo date('Y'); ?> Daniel Velez. All Rights Reserved.</p>   
+            
+            <!--Terms and policy link container-->
+            <div class="terms-and-policy">
+                <a href="terms.php">Terms of Service</a>
+                <a href="policy.php">Privacy Policy</a>
+            </div>
+        </div>
     </footer>
 </body>
 </html>
