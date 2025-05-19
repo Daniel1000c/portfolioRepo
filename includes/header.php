ad3de8cbbb251750ee38b1642f42d556e4d6ed28
Daniel Velez <velezdaniel516@gmail.com>
2025-05-19 15:58:46 -0400

    add link to favicon

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Porfolio</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="favicon/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"><!--Link to boxicons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous"><!--Link to bootstrap css-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script><!--Link to bootstrap js-->
