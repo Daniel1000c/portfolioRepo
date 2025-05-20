2b59fd2172e626fc4835fff9b1e24b93d7bb81d8
Daniel Velez <velezdaniel516@gmail.com>
2025-05-20 18:56:33 -0400

    create contact section php component

---
+++ includes/contactSection.php
@@ -0,0 +1,7 @@
+<section class="contact">
+    <div class="container">
+        <h2>Get In Touch</h2>
+        <p>Looking to start a new project? We'd love to hear from you!</p>
+        <a href="contact.php">Send a Message</a>
+    </div>
+</section>
