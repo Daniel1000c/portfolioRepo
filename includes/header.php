4df32698183e4059f095b47a1035b0fe8b2da26a
Daniel Velez <velezdaniel516@gmail.com>
2025-05-20 18:58:43 -0400

    add active php class toggle feature

--- includes/header.php
+++ includes/header.php
@@ -18,26 +18,26 @@
             <!--Create navbar-->
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="index.php">Daniel Velez</a>
+                    <a class="navbar-brand" href="index.php">Daniel <span>Velez</span></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse justify-content-center justify-content-lg-between" id="navbarSupportedContent">
                     <ul class="navbar-nav mb-2 mb-lg-0 mx-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                            <a class="nav-link <?php if(!empty($pageKey) && $pageKey == 'home') echo 'active'; ?>" aria-current="page" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="about.php">About</a>
+                            <a class="nav-link <?php if(!empty($pageKey) && $pageKey == 'about') echo 'active'; ?>" href="about.php">About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="service.php">Services</a>
+                            <a class="nav-link <?php if(!empty($pageKey) && $pageKey == 'service') echo 'active'; ?>" href="service.php">Services</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="projects.php">Projects</a>
+                            <a class="nav-link <?php if(!empty($pageKey) && $pageKey == 'projects') echo 'active'; ?>" href="projects.php">Projects</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="contact.php">Contact</a>
+                            <a class="nav-link <?php if(!empty($pageKey) && $pageKey == 'contact') echo 'active'; ?>" href="contact.php">Contact</a>
                         </li>
                     </ul>
 
