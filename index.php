eb0f02fb7f41cf4120a32205c0a8ea5e168a8f29
Daniel Velez <velezdaniel516@gmail.com>
2025-05-21 16:42:21 -0400

    add hero section template

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
 
     //Include header.php
     require 'includes/header.php';
-?>
+?>  
+
+<!--Hero Section-->
+<section class="hero">
+    <div class="container">
+        <h2>Hi, I'm <span>Daniel Velez</span></h2>
+    </div>
+</section>
 
 <?php
     //Include contact.php
