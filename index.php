fc50fe7ac928bac69055f0d253c430e3755e9024
Daniel Velez <velezdaniel516@gmail.com>
2025-05-22 15:10:09 -0400

    modify page title

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
 <section class="hero">
     <div class="container">
         <h2>Hi, I'm <span>Daniel Velez</span></h2>
+
+        <!--Button container-->
+        <div class="btnContainer">
+            <a href="pdf/Daniel_Velez_Resume.pdf" download="Daniel_Velez_Resume.pdf">Resume</a>
+            <a href="projects.php">Projects</a>
+        </div>
     </div>
 </section>
 
