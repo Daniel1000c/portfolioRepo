626140678862e90eab606198087a2d80e48abe3e
Daniel Velez <velezdaniel516@gmail.com>
2025-06-02 15:31:21 -0400

    add about us text and quick links

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,31 @@
 </main>
     <footer>
+        <!--About and site quick links-->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 footerAbout">
+                    <h3>Daniel <span>Velez</span></h3>
+                    <p>Turning Ideas into Interactive, Scalable Web Experiences.</p>
+
+                    <!--Social media icons container-->
+                    <div class="socialIcons">
+                        <a href="https://www.linkedin.com/in/daniel-velez-b17748286/" target="_blank"><i class="bx bxl-linkedin"></i></a>
+                        <a href="mailto:velezdaniel6162gmail.com"><i class="bx bxl-gmail"></i></a>
+                        <a href="https://codepen.io/Daniel-Velez-the-looper" target="_blank"><i class="bx bxl-codepen"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 footerQuickLinks">
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="projects.php">Projects</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
          <hr>
         <div class="container">
            
